c9ac6603be77421391cd93c6ef1d992f3c5a342e
alexandre-v1 <vivona.alexandre@gmail.com>
2024-11-13 21:27:27 +0100

    Refactor Main.cs

--- Flow.Launcher.Plugin.Obsidian/Main.cs
+++ Flow.Launcher.Plugin.Obsidian/Main.cs
@@ -1,21 +1,52 @@
-using System;
 using System.Collections.Generic;
+using System.Linq;
 using Flow.Launcher.Plugin;
 
 namespace Flow.Launcher.Plugin.Obsidian
 {
     public class Obsidian : IPlugin
     {
-        private PluginInitContext _context;
+        private PluginInitContext? _context;
+        private IPublicAPI? _publicApi;
 
         public void Init(PluginInitContext context)
         {
             _context = context;
+            _publicApi = context.API;
+            VaultManager.UpdateVaultList(_publicApi);
+            
         }
 
         public List<Result> Query(Query query)
         {
-            return new List<Result>();
+            var helloWorldResult = new Result
+            {
+                Title = "Hello World",
+                SubTitle = "This is a subtitle",
+                IcoPath = "Images\\app.png",
+                Action = e =>
+                {
+                    _context?.API.ShowMsg("Hello World");
+                    return true;
+                }
+            };
+            
+            List<string> allFiles = new();
+            foreach (Vault vault in VaultManager.Vaults)
+            {
+                if (vault.Files != null) allFiles.AddRange(vault.Files);
+            }
+            
+            var resultsMatches = StringMatcher.FindClosestMatches(allFiles, query.Search, maxDistance: 2);
+
+            string? debug = ""; 
+            resultsMatches = resultsMatches.Take(10).ToList();
+            foreach ((string? text, int distance) in resultsMatches)
+            {
+                debug += text + " \n";
+            }
+            _context?.API.ShowMsg(debug);
+            return resultsMatches.Select(variable => new Result { Title = variable.Text, }).ToList();
         }
     }
 }
