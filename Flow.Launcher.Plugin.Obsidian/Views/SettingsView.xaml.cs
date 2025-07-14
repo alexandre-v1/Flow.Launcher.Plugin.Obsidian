d68af2e64ddb7c0ccf2d8fcd1a26ba03fd4e4300
alexandre-v1 <101522434+alexandre-v1@users.noreply.github.com>
2025-07-14 12:22:29 +0200

    Delete not used views

--- Flow.Launcher.Plugin.Obsidian/Views/SettingsView.xaml.cs
+++ Flow.Launcher.Plugin.Obsidian/Views/SettingsView.xaml.cs
@@ -1,88 +1,21 @@
-using System.ComponentModel;
-using System.Linq;
-using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Windows;
-using Flow.Launcher.Plugin.Obsidian.Managers;
-using Flow.Launcher.Plugin.Obsidian.Models;
-using CheckBox = System.Windows.Controls.CheckBox;
+using Flow.Launcher.Plugin.Obsidian.Services;
+using Flow.Launcher.Plugin.Obsidian.ViewModels;
 
 namespace Flow.Launcher.Plugin.Obsidian.Views;
 
-public partial class SettingsView : INotifyPropertyChanged
+public partial class SettingsView
 {
-    public int MaxResults
-    {
-        get => _maxResults;
-        set
-        {
-            if (_maxResults == value) return;
-            _maxResults = value;
-            Settings.MaxResult = value;
-            OnPropertyChanged();
-        }
-    }
+    private readonly SettingsViewModel _viewModel;
 
-    private Obsidian Obsidian { get; }
-    private Settings Settings { get; }
-    private int _maxResults;
-
-    public SettingsView(VaultManager vaultManager, Obsidian obsidian)
+    public SettingsView(SettingsViewModel viewModel)
     {
-        Obsidian = obsidian;
-        Settings = vaultManager.Settings;
         InitializeComponent();
-        CreateVaultSettingControls(vaultManager);
+        _viewModel = viewModel;
+        DataContext = _viewModel;
     }
 
-    private void SettingsView_OnLoaded(object sender, RoutedEventArgs e)
-    {
-        MaxResults = Settings.MaxResult;
-        SetupCheckboxes();
-    }
+    private void OnLoaded(object sender, RoutedEventArgs e) => StyleManager.Instance.ApplyStylesToControl(this);
 
-    private void SetupCheckboxes()
-    {
-        SetupCheckbox(AddCreateNoteOptionOnAllSearch, nameof(Settings.AddCreateNoteOptionOnAllSearch));
-        SetupCheckbox(UseFileExtension, nameof(Settings.UseFilesExtension));
-        SetupCheckbox(UseAliases, nameof(Settings.UseAliases));
-        SetupCheckbox(UseTags, nameof(Settings.UseTags));
-        SetupCheckbox(AddGlobalFolderExcludeToContext, nameof(Settings.AddGlobalFolderExcludeToContext));
-        SetupCheckbox(AddLocalFolderExcludeToContext, nameof(Settings.AddLocalFolderExcludeToContext));
-        SetupCheckbox(AddCheckBoxesToContext, nameof(Settings.AddCheckBoxesToContext));
-    }
-
-    private void SetupCheckbox(CheckBox checkBox, string propertyName)
-    {
-        PropertyInfo? property = Settings.GetType().GetProperty(propertyName);
-        if (property is null) return;
-        checkBox.IsChecked = property.GetValue(Settings) as bool? ?? false;
-        checkBox.Checked += (_, _) => property.SetValue(Settings, true);
-        checkBox.Unchecked += (_, _) => property.SetValue(Settings, false);
-    }
-
-    private void SettingsView_OnUnloaded(object sender, RoutedEventArgs e) => _ = Obsidian.ReloadDataAsync();
-
-    private void CreateVaultSettingControls(VaultManager vaultManager)
-    {
-        GlobalVaultSettingView globalVaultSettingControl = new(vaultManager);
-        GlobalVaultSettingPanel.Children.Add(globalVaultSettingControl);
-        Thickness margin = new(0, 0, 10, 0);
-        foreach (VaultSettingView? vaultSettingControl in vaultManager.Vaults.Select(vault =>
-                     new VaultSettingView(vaultManager, vault) { Margin = margin }))
-            VaultsSettingPanel.Children.Add(vaultSettingControl);
-    }
-
-    private void OnIncrease(object sender, RoutedEventArgs e) => MaxResults++;
-
-    private void OnDecrease(object sender, RoutedEventArgs e)
-    {
-        MaxResults--;
-        if (MaxResults < 0) MaxResults = 0;
-    }
-
-    private void OnPropertyChanged([CallerMemberName] string? propertyName = null) =>
-        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-
-    public event PropertyChangedEventHandler? PropertyChanged;
+    private void OnUnloaded(object sender, RoutedEventArgs e) => _ = _viewModel.ReloadPluginDataAsync();
 }
